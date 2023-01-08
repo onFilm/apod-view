b4d760182af5f28f7761bcba073004da0ca57de0
Prajwal Acharya <mail2prajwal12@gmail.com>
2023-01-08 20:24:27 +0530

    refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContainerComponent } from './container/container.component';
 
 const routes: Routes = [
-  { path: '', component: ContainerComponent }
+  { path: 'container', component: ContainerComponent }
 ];
 
 @NgModule({
--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { PaginatePipe } from 'ngx-pagination';
 import { ApodDataService } from '../apod-data.service';
 
 @Component({
